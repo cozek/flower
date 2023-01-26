1b6fe306cf4755da597f8df27c33b66b6025eab7
Kaushik Amar Das <cozek@users.noreply.github.com>
2023-01-26 12:57:19 +0000

    updates

--- examples/sklearn-nn/nn-try.py
+++ examples/sklearn-nn/nn-try.py
@@ -6,75 +6,42 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
 import flwr as fl
 
-def get_model_parameters(model):
-    return [
-        model.coefs_,
-        model.intercepts_
-    ]
+# (X, y), (X_test, y_test) = utils.load_mnist()
 
-def set_model_params(model, params, config):
-    model.coefs_= params.coefs_.copy()
-    model.intercepts_ = params.intercepts_.copy()
-
-    hidden_layer_sizes = config['hidden_layer_sizes']
-    layer_units = [0] + hidden_layer_sizes + [0]
-    
-    model.n_layers_ = len(layer_units)
-    model.out_activation_ = params.out_activation_
-    # clf_2.set_params(**clf.get_params())
-
-    
-
-class MnistNNClient(fl.client.NumPyClient):
-        def __init__(self) -> None:
-            super().__init__()
-            self.model = None
-        
-        def get_parameters(self, config):  # type: ignore
-            # create a random model
-            if self.model is None:
-                self.model = MLPClassifier(**config['initial_model_config'])
-            return get_model_parameters(self.model)
-
-        def fit(self, parameters, config):  # type: ignore
-            utils.set_model_params(model, parameters, config)
-            # Ignore convergence failure due to low local epochs
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                model.fit(X_train, y_train)
-            print(f"Training finished for round {config['server_round']}")
-            return utils.get_model_parameters(model), len(X_train), {}
-
-        def evaluate(self, parameters, config):  # type: ignore
-            utils.set_model_params(model, parameters)
-            loss = log_loss(y_test, model.predict_proba(X_test))
-            accuracy = model.score(X_test, y_test)
-            return loss, len(X_test), {"accuracy": accuracy}
-
-if __name__ == '__main__':
-
-    (X, y), (X_test, y_test) = utils.load_mnist()
-
-    # Split train set into 10 partitions and randomly use one for training.
-    partition_id = np.random.choice(10)
-    (X, y) = utils.partition(X, y, 10)[partition_id]
+# Split train set into 10 partitions and randomly use one for training.
+# partition_id = np.random.choice(10)
+# (X, y) = utils.partition(X, y, 10)[partition_id]
 
 
 
-# X = np.array([[0., 0.], [1., 1.]])
-# y = np.array([0, 1])
+X = np.array([[0., 0.], [1., 1.]])
+y = np.array([0, 1])
 
 # # Ensure y is 2D
 # if y.ndim == 1:
 #     y = y.reshape((-1, 1))
 
 
-model = MLPClassifier(warm_start=True)
+model = MLPClassifier(hidden_layer_sizes=(4,2,2,5),warm_start=True)
 
-# clf.fit(X, y)
+print(model.get_params()['hidden_layer_sizes'])
 
-# # print(clf.coefs_)
-# # print(len(clf.coefs_))
+
+model.fit(X, y)
+
+params = model.coefs_ + model.intercepts_
+print(len(model.coefs_))
+print(len(model.intercepts_))
+
+print(len(params))
+
+# print(model.coefs_)
+# print(len(model.coefs_))
+
+# print(model.get_params())
+# print(model.out_activation_)
+
+
 # for a in clf.coefs_:
 #     print(a.shape)
 # print(clf.n_layers_)
--- examples/sklearn-nn/nn_client.py
+++ examples/sklearn-nn/nn_client.py
@@ -7,39 +7,46 @@ from sklearn.metrics import log_loss
 import numpy as np
 import flwr as fl
 
+MODEL_CONFIG = {'hidden_layer_sizes': (10, 2), 'max_iter': 100, 'warm_start':True}
 
 def get_model_parameters(model):
-    # TODO: Find appropriate way to get model params
-    return [
-        model.coefs_,
-        model.intercepts_
-    ]
+    return model.coefs_ + model.intercepts_
 
 def set_model_params(model, params, config):
-    # TODO: actually set model_params
-    model.coefs_= params.coefs_.copy()
-    model.intercepts_ = params.intercepts_.copy()
+    n = model.get_params()['hidden_layer_sizes']
+    model.coefs_= params[:n+1]
+    model.intercepts_ = params[n+1:]
+    model.n_layers_ = n+2
+    # Output for multi class "softmax"
+    # Output for binary class and multi-label: "logistic"
+    model.out_activation_ = 'logistic'
 
-    hidden_layer_sizes = config['hidden_layer_sizes']
+def initialize_model():
+    model = MLPClassifier(**MODEL_CONFIG)
+    hidden_layer_sizes = model.get_params()['hidden_layer_sizes']
     layer_units = [0] + hidden_layer_sizes + [0]
-    
     model.n_layers_ = len(layer_units)
-    model.out_activation_ = params.out_activation_
-
+    # Output for multi class "softmax"
+    # Output for binary class and multi-label: "logistic"
+    model.out_activation_ = 'logistic'
+    return model
 
 class MnistNNClient(fl.client.NumPyClient):
-        def __init__(self, X_train,y_train, X_test,y_test) -> None:
+        def __init__(self, X_train, y_train, X_test, y_test) -> None:
             super().__init__()
             self.model = None
         
         def get_parameters(self, config):  # type: ignore
-            # create a random model
+            
+            # server might request params and there may not be a model yet
             if self.model is None:
-                self.model = MLPClassifier(**config['initial_model_config'])
+                self.model = initialize_model()
             return get_model_parameters(self.model)
 
         def fit(self, parameters, config):  # type: ignore
-            utils.set_model_params(self.model, parameters, config)
+            if self.model is None:
+                self.model = initialize_model()
+            set_model_params(self.model, parameters, config)
             # Ignore convergence failure due to low local epochs
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore")
